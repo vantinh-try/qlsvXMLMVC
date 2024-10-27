41bc6af4bdcff6789a3aebd0816a999c6d2aaca7
Văn Tịnh <vantinh@VanTinh.local>
2024-10-27 23:27:42 +0700

    first commit 2

--- WebApplicationXml/obj/Debug/net8.0/WebApplicationXml.AssemblyInfo.cs
+++ WebApplicationXml/obj/Debug/net8.0/WebApplicationXml.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApplicationXml")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3178528c4256b3f6376c54e1b8a8c8880f4aca96")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApplicationXml")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApplicationXml")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
